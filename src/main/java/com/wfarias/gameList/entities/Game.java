2a427cda14f9371d039d014280546630d70d23ae
wfarias <farias.william@outlook.com>
2023-07-12 12:18:23 -0400

    properties, Game ORM

---
+++ src/main/java/com/wfarias/gameList/entities/Game.java
@@ -0,0 +1,61 @@
+package com.wfarias.gameList.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_game")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Game {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+	
+/*	
+ * Em caso de não usar o Lombok também se pode usar assim
+ *
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	} 
+	*/
+}
