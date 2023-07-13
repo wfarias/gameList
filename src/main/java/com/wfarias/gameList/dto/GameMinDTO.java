b4de866ffda67314fad4ff94dba9edc2bc67fa3e
wfarias <farias.william@outlook.com>
2023-07-13 18:11:04 -0400

    Homolog ok

--- src/main/java/com/wfarias/gameList/dto/GameMinDTO.java
+++ src/main/java/com/wfarias/gameList/dto/GameMinDTO.java
@@ -10,7 +10,7 @@ public record GameMinDTO(Long id,String title, Integer year, String imgUrl,  Str
 	}
 	
 	public GameMinDTO(GameMinProjection projection){
-		this(projection.getId(),projection.getTitle(),projection.getYear(),
+		this(projection.getId(),projection.getTitle(),projection.getGameYear(),
 			 projection.getImgUrl(),projection.getShortDescription());
 	}
 	
